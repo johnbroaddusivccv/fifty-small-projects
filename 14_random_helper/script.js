8afa785bb9ed4a4defa369b5ad8faac58bddfc43
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2021-07-16 06:42:17 -0500

    enter

--- 14_random_helper/script.js
+++ 14_random_helper/script.js
@@ -5,3 +5,27 @@ toggles.forEach((toggle) => {
     toggle.classList.toggle('highlight')
   })
 })
+
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+})
+
+function createTags(input) {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim())
+  tagsEl.innerHTML = ''
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span')
+    tagEl.classList.add('tag')
+    tagEl.innerHTML = tag
+    tagsEl.appendChild(tagEl)
+  })
+}
