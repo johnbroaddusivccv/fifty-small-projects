3fc573b1d782126b85c6b232235fe7371ca351bf
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2021-07-16 06:56:09 -0500

    javascript

--- 14_random_helper/script.js
+++ 14_random_helper/script.js
@@ -13,8 +13,51 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+    randomSelect()
+  }
 })
 
+function randomSelect() {
+  const times = 30
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    highlightTag(randomTag)
+
+    setTimeout(() => {
+      unHighlightTag(randomTag)
+    }, 100)
+  }, 100)
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
+}
 function createTags(input) {
   const tags = input
     .split(',')
