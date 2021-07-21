fe7386b044a25480ffb40422f52b20c8c5a1f16f
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2021-07-21 15:07:03 -0500

    script

--- 16_Increment_counter/script.js
+++ 16_Increment_counter/script.js
@@ -11,6 +11,9 @@ counters.forEach((counter) => {
 
     if (c < target) {
       counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+    } else {
+      counter.innerText = target
     }
   }
   updateCounter()
